ccbebff758a48ae09ac986c33ba5d923b7a5c766
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-19 01:10:08 +0600

    create interface and route

--- src/app/modules/Products/product.route.ts
+++ src/app/modules/Products/product.route.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
+import { productController } from './product.controller';
 
 const router = Router();
 
-// router.get('/', productController.getAllProducts);
-// router.post('/', productController.createProduct);
-// router.get('/:id', productController.getProductById);
-// router.patch('/:id', productController.updateProduct);
-// router.delete('/:id', productController.deleteProduct);
+router.post('/', productController.createProduct);
 
 export const productRouter = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 const moduleRoutes = [
   {
-    path: '',
+    path: '/products',
     route: productRouter,
   },
 ];
