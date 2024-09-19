ec5ec7e879a211b5d41b6301bf97d603a2263bf9
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-19 14:11:05 +0600

    create post products route

--- src/app/modules/Products/product.route.ts
+++ src/app/modules/Products/product.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { productController } from './product.controller';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { productsValidation } from './product.validations';
 
 const router = Router();
 
-router.post('/', productController.createProduct);
+router.post(
+  '/',
+  validateRequest(productsValidation.ProductSchema),
+  productController.createProduct,
+);
 
 export const productRouter = router;
