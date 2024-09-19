31bcf810398b5c66b572eb6967bf248a237abd5c
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-19 22:24:07 +0600

    create product router

--- src/app/modules/Products/product.route.ts
+++ src/app/modules/Products/product.route.ts
@@ -10,5 +10,9 @@ router.post(
   //   validateRequest(productsValidation.ProductSchema),
   productController.createProduct,
 );
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.patch('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 export const productRouter = router;
