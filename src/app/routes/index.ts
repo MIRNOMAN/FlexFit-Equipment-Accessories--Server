d9d880a23f72100b0cc0918057337a9c4ea008e2
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-26 15:33:59 +0600

    create index router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { productRouter } from '../modules/Products/product.route';
+import { AuthRoutes } from '../modules/user/user.route';
 
 const router = Router();
 
@@ -7,6 +8,10 @@ const moduleRoutes = [
   {
     path: '/products',
     route: productRouter,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
   },
 ];
 
