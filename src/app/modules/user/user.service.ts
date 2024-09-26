e68dbd2c28496b32ae78efae79bd739d80395497
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-26 15:28:33 +0600

    user service create user, login, getuser

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,57 @@
+import httpStatus from 'http-status';
+import { TLoginInfo, TUser } from './user.interface';
+import { User } from './user.model';
+import { AppError } from '../../error/appError';
+import { createToken } from './user.utils';
+import config from '../../config';
+
+const createUserIntoDB = async (payload: TUser) => {
+  const result = await User.create(payload);
+  return result;
+};
+
+const loginUser = async (payload: TLoginInfo) => {
+  const user = await User.isUserExistByEmail(payload?.email);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'this user is not found!');
+  }
+
+  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Password does not match!');
+  }
+
+  const jwtPayload = {
+    userId: user?._id,
+  };
+
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+    user,
+  };
+};
+
+const getUserFromDB = async (id: string) => {
+  const result = await User.findById(id);
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, 'User not found');
+  }
+  return result;
+};
+export const UserServices = {
+  createUserIntoDB,
+  loginUser,
+  getUserFromDB,
+};
