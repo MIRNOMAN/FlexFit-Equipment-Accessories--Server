054cad3f8374b6123121c1e6c3c2624de01481dc
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-26 15:33:04 +0600

    create router singup login and getuser

---
+++ src/app/modules/user/user.route.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { UserValidation } from './user.validation';
+import { UserControllers } from './user.controller';
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  validateRequest(UserValidation.userValidationSchema),
+  UserControllers.createUser,
+);
+
+router.post(
+  '/login',
+  validateRequest(UserValidation.LoginValidationSchema),
+  UserControllers.loginUser,
+);
+
+router.get('/user', UserControllers.getUser);
+
+export const AuthRoutes = router;
