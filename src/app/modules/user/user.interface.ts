1ab962b1022c3dc39798191ee6ecf3f812ba6d06
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-09-26 14:55:10 +0600

    create user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,27 @@
+import { Model, ObjectId } from 'mongoose';
+import { USER_ROLE } from './auth.constant';
+
+export type TUser = {
+  _id: ObjectId;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type TLoginInfo = {
+  email: string;
+  password: string;
+};
+export interface UserModel extends Model<TUser> {
+  isUserExistByCustomId(id: string): Promise<TUser>;
+  isJwtIssuedBeforePasswordChanged(
+    passwordChangedTimeStamp: Date,
+    jwtIssuedTimeStamp: number,
+  ): boolean;
+  isUserExistByEmail(email: string): Promise<TUser | null>;
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+}
+export type TUserRole = keyof typeof USER_ROLE;
